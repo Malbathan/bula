195d95a1bfd2791703cb6e5a9e720da326126647
Malbathan <malbathanlima@gmail.com>
2022-11-26 12:12:53 -0300

    fix:

--- src/components/UI/resultSearch.js
+++ src/components/UI/resultSearch.js
@@ -1,43 +1,19 @@
 import styled from "styled-components"
 
 export const ResultBox = styled.div`
-  background: white;
+  background: #f1f1f1;
   border-radius: 5px;
 
   max-height: 300px;
-
+  overflow:hidden;
+  overflow-y: auto;
   position:relative;
   margin-top: 10px;
-  border: 4px solid #00bfb6;
 
   &:webkit-scrollbar {
     display: none
   }
-  &:before {
-    content: "";
-    width: 0px;
-    height: 0px;
-    position: absolute;
-    border-left: 10px solid transparent;
-    border-right: 10px solid transparent;
-    border-top: 10px solid transparent;
-    border-bottom: 10px solid #00bfb6;
-    right: 50%;
-    top: -23px;
-  }
   
-  &:after {
-    content: "";
-    width: 0px;
-    height: 0px;
-    position: absolute;
-    border-left: 10px solid transparent;
-    border-right: 10px solid transparent;
-    border-top: 10px solid transparent;
-    border-bottom: 10px solid #fff;
-    right: 50%;
-    top: -18px;
-  }
 `
 
 export const Item = styled.div `
